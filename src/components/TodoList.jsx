0eadfe5544d0d04150d71230cdbd6d47a4f998a5
alfe <alfe10251@gmail.com>
2021-12-08 00:44:30 +0900

    yarn add react-scripts

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,27 +6,30 @@ const USER_ID = window.location.hash.replace('#', '') || 'XXXXX';
 
 const TodoList = ({ db }) => {
   const [list, setList] = useState([]);
-  useEffect(() => {
-    getList();
-  }, []);
-  const getList = async () => {
-    const docSnap = await getDoc(doc(db, "todo", USER_ID));
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-      setList(
-        Object.entries(docSnap.data())
-          .map(([id, value]) => ({ id, value }))
-          .sort((a, b) => Number(b.id) - Number(a.id))
-      );
-    }
-  };
+  // useEffect(() => {
+  //   getList();
+  // }, []);
+  // const getList = async () => {
+  //   const docSnap = await getDoc(doc(db, "todo", USER_ID));
+  //   if (docSnap.exists()) {
+  //     console.log("Document data:", docSnap.data());
+  //     setList(
+  //       Object.entries(docSnap.data())
+  //         .map(([id, value]) => ({ id, value }))
+  //         .sort((a, b) => Number(b.id) - Number(a.id))
+  //     );
+  //   }
+  // };
   const addTodo = async (value) => {
-    await setDoc(doc(db, "todo", USER_ID), { [new Date().getTime()]: value }, { merge: true });
-    getList();
+    // await setDoc(doc(db, "todo", USER_ID), { [new Date().getTime()]: value }, { merge: true });
+    // getList();
+    setList([...list, { id: [new Date().getTime()], value }]);
   }
   const deleteTodo = async (id) => {
-    await updateDoc(doc(db, "todo", USER_ID), { [id]: deleteField() });
-    getList();
+    // await updateDoc(doc(db, "todo", USER_ID), { [id]: deleteField() });
+    // getList();
+
+    setList(list.filter(i => i.id !== id))
   }
   return (
     <>
